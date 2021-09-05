bd467b9afa9256b9e9f1d60930cf92f84cec7d81
videokurs12 <airincars@gmail.com>
2021-09-05 15:01:52 +0300

    Закончил Часть 1 Урок №4

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::get('/', function () {
+    $res = 2+3;
+    $name = 'John';
+    $var = 1;
+    return view('home', compact('res', 'name', 'var'));
 });
+
+Route::get('/about', function () {
+    return 'About';
+});
+
