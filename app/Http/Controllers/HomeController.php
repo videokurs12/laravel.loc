192237a095b476b645ef96bfc92215d5a815db00
videokurs12 <airincars@gmail.com>
2021-09-06 15:30:36 +0300

    Закончил Часть 1 Урок №9

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,6 +8,11 @@ class HomeController extends Controller
 {
     public function index()
     {
+        dump($_ENV['MY_SETTING']);
+        dump(env('MY_SETTING2', '111'));
+        dump(config('database.connections.mysql.database'));
+        dump($_ENV);
+
         return view('home', ['res' => 5, 'name' => 'John']);
     }
 
