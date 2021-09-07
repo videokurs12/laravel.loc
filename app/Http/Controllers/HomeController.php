da4d71f97ae6c7f21eb38258cb775efd2fd13038
videokurs12 <airincars@gmail.com>
2021-09-07 17:53:10 +0300

    Закончил Часть 1 Урок №13

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,18 +4,20 @@
 namespace App\Http\Controllers;
 
 
+use App\Models\Post;
 use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
     public function index()
     {
-//        $data = DB::table('city')->select('CountryCode')->distinct()->get();
+        $post = new Post();
+        $post->title = 'Статья 2';
+//        $post->content = 'Контент Статья 1';
+        $post->save();
 
 
-
-
-
+        return view('home');
     }
 
     public function test()
